7c539793fd8f2fe767f86d0eb945177d32a3d7a6
ameliarider <mimirider89@gmail.com>
2025-07-15 11:02:08 -0700

    Init + member model + members controller

---
+++ db/migrate/20250714193718_create_members.rb
@@ -0,0 +1,13 @@
+class CreateMembers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :members do |t|
+      t.string :first_name
+      t.string :last_name
+      t.date :dob
+      t.string :external_member_id
+      t.string :zip
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+require 'ffaker'
+
+i = 0
+99.times do
+  member = Member.create(
+    first_name: FFaker::Name.first_name,
+    last_name: FFaker::Name.last_name,
+    dob: FFaker::Date.birthday,
+    external_member_id: "1111111#{i}",
+    zip: FFaker::AddressUS.zip_code
+  )
+  i+=1
+end
