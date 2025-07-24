1c108510e36d48dfa7ac5b67aba3726432fe7a1d
ameliarider <mimirider89@gmail.com>
2025-07-24 08:44:29 -0700

    Fixed lookup logic and simplified code

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -12,8 +12,8 @@ class MembersController < ApplicationController
       @member = Member.find_by(external_member_id: params[:external_member_id])
       if @member
         render json: @member, status: :ok, message: "Active"
-      elsif params[:first_name] && params[:last_name] && params[:dob] && params[:zip]
-        @member = Member.find_by(first_name: params[:first_name], last_name: params[:last_name], dob: params[:dob], zip: params[:zip])
+      elsif required_params.present?
+        @member = Member.find_by(search_params)
         if @member
           render json: @member, status: :ok, message: "Active"
         else
@@ -22,11 +22,24 @@ class MembersController < ApplicationController
       else
         render json: { message: "Please provide all required fields" }
       end
-    elsif params[:first_name] && params[:last_name] && params[:dob] && params[:zip]
-      @member = Member.where(first_name: params[:first_name])
-      render json: @member, status: :ok, message: "Active"
+    elsif required_params.present?
+      @member = Member.find_by(search_params)
+      if @member
+        render json: @member, status: :ok, message: "Active"
+      else
+        render json: { message: "Unable to find member" }, status: :not_found
+      end
     else
-      render json: { message: "Unable to verify member eligibility" }
+      render json: { message: "Please provide all required fields" }
     end
+  end
+
+  private
+  def required_params
+    required_params = params[:first_name] && params[:last_name] && params[:dob] && params[:zip]
+  end
+
+  def search_params
+    search_params = { first_name: params[:first_name], last_name: params[:last_name], dob: params[:dob], zip: params[:zip] }
   end
 end
