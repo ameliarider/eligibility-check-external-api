05efc3fccf080ffc1873f9a7b7bddd2023090f51
ameliarider <mimirider89@gmail.com>
2025-07-23 17:48:52 -0700

    Add show action matching criteria - first name, last name, dob, and zip

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -8,13 +8,25 @@ class MembersController < ApplicationController
   end
 
   def show
-    @member = Member.find_by(external_member_id: params[:external_member_id])
-
-    if @member
+    if params[:external_member_id].present?
+      @member = Member.find_by(external_member_id: params[:external_member_id])
+      if @member
+        render json: @member, status: :ok, message: "Active"
+      elsif params[:first_name] && params[:last_name] && params[:dob] && params[:zip]
+        @member = Member.find_by(first_name: params[:first_name], last_name: params[:last_name], dob: params[:dob], zip: params[:zip])
+        if @member
+          render json: @member, status: :ok, message: "Active"
+        else
+          render json: { message: "Unable to find member" }, status: :not_found
+        end
+      else
+        render json: { message: "Please provide all required fields" }
+      end
+    elsif params[:first_name] && params[:last_name] && params[:dob] && params[:zip]
+      @member = Member.where(first_name: params[:first_name])
       render json: @member, status: :ok, message: "Active"
     else
-      @member = Member.find_by_sql("SELECT * FROM members WHERE first_name = '#{params[:first_name]}'AND last_name = '#{params[:last_name]}'")
-      render json: { message: "Found via alternative method" }
+      render json: { message: "Unable to verify member eligibility" }
     end
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -4,5 +4,5 @@ class Member < ApplicationRecord
     validates :dob, presence: true
     validates :external_member_id, presence: true, uniqueness: true
     validates :zip, presence: true
-    encrypts :first_name, :last_name
+    encrypts :first_name, :last_name, deterministic: true
 end
